4a845dc829ea03f86a8a6c57a95e3fee506c26c8
MrSa2660 <Salih2660akkaymak@gmail.com>
2023-08-21 13:36:33 +0200

    commit

---
+++ Fodbold.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeCases
+{
+    internal class Fodbold
+    {
+        static void Main(string[] args)
+        {
+
+            
+            string goal = Console.ReadLine().ToLower();
+
+
+            Console.WriteLine("Hvor mange afleveringer");
+            int.TryParse(Console.ReadLine(), out int Passes);
+
+
+            if (goal == "mål")
+            {
+                Console.WriteLine("Olé olé olé");
+            } else if (Passes >= 1 && Passes <= 10)
+            {
+
+            }
+        }
+    }
+}
